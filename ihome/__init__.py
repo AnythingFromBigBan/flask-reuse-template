4f2468b1df044452475588ec3bc1acc06ac8e540
zhangzz <zhangzz7998@163.com>
2018-10-26 12:39:26 +0800

    集成日志

--- ihome/__init__.py
+++ ihome/__init__.py
@@ -1,3 +1,6 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
 from flask import Flask
 from flask_migrate import Migrate
 from flask_session import Session
@@ -9,6 +12,20 @@ from config import config_dict
 # 将数据库连接对象全局化(方便其他文件使用)
 db = None
 sr = None
+
+
+# 日志配置(将日志保存到文件中)
+def setup_log():
+    # 设置日志的记录等级
+    logging.basicConfig(level=logging.DEBUG)  # 调试debug级
+    # 创建日志记录器，指明日志保存的路径、每个日志文件的最大大小、保存的日志文件个数上限
+    file_log_handler = RotatingFileHandler("logs/log", maxBytes=1024 * 1024 * 100, backupCount=10)
+    # 创建日志记录的格式 日志等级 输入日志信息的文件名 行数 日志信息
+    formatter = logging.Formatter('%(levelname)s %(pathname)s:%(lineno)d %(message)s')
+    # 为刚创建的日志记录器设置日志记录格式
+    file_log_handler.setFormatter(formatter)
+    # 为全局的日志工具对象（flask app使用的）添加日志记录器
+    logging.getLogger().addHandler(file_log_handler)
 
 
 # 定义函数 封装应用的创建和配置  工厂函数(调用者提供生产物料, 函数内部封装创建过程)
@@ -39,4 +56,8 @@ def create_app(config_type):
 
     # 3. 注册蓝图
     app.register_blueprint(html_blu)
+
+    # 配置日志
+    setup_log()
+
     return app
--- ihome/modules/html/views.py
+++ ihome/modules/html/views.py
@@ -1,9 +1,13 @@
 from ihome import sr
 from ihome.modules.html import html_blu
+import logging  # python内置的日志模块  将日志信息在控制台中输出, 并且可以将日志保存到文件中
+# flask中的默认日志也是集成的logging模块, 但是没有将日志保存到文件中
+from flask import current_app
 
 
 # 2.使用蓝图注册路由
 @html_blu.route('/')
 def index():
-    sr.set("name", "lisi")
+    # logging.error("出现了一个错误")  # logging默认的输出不包含错误位置, 显示效果不好, 可以使用flask内置的日志输出语法来代替
+    current_app.logger.error("出现了一个错误")
     return "index"
