911dc0e7b42f690dde6c92b78e5ba8454da7f36c
zhangzz <zhangzz7998@163.com>
2018-10-26 12:09:22 +0800

    基本配置和mysql配置

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+
+class Config:
+    # 设置配置同名的类属性
+    DEBUG = True  # 设置调试模式
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/ihome"  # 设置数据库连接地址
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # 是否追踪数据库变化
+
+
+app.config.from_object(Config)
+# 创建数据库连接
+db = SQLAlchemy(app)
 
 
 @app.route('/')
@@ -10,4 +23,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
